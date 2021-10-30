0985bc292f9e0f3a9331ee23264d2c9806fe45a5
theo923 <theotam923@gmail.com>
2021-10-31 04:38:44 +0800

    test web chrome2

--- interface/interface.ts
+++ interface/interface.ts
@@ -4,3 +4,22 @@ export interface childNode {
     };
     children?: React.ReactNode;
 }
+
+export interface booksProps {
+    asin: string;
+    title: string;
+    image: string;
+    authors: string[];
+    description: string;
+    stars: string;
+    language: string;
+    file_size: string;
+    publisher: string;
+    publication_date: string;
+    text_to_speech: string;
+    x_ray: string;
+    word_wise: string;
+    print_length: string;
+    best_sellers_rank: string;
+    customer_reviews: string;
+}
