04f7c8fb9735c1d72c1540a5bd4ada0ad2d5c4fb
theo923 <theotam923@gmail.com>
2021-10-31 22:05:34 +0800

    fixes, added tutorial & timer

--- pages/api/amazon.js
+++ pages/api/amazon.js
@@ -3,17 +3,17 @@ const { amazonRequest } = require("../../scripts/amazon");
 
 export default async function amazonAPI(req, res) {
     const { url } = req.body;
+    // let request;
+    // let attempt = 0;
     try {
-        let request;
-        let attempt = 0;
-        do {
-            console.log("Request");
-            request = await amazonRequest(url);
-            console.log("EndRequest");
-            attempt++;
-        } while (attempt <= 5 && Object.keys(request).length === 0);
+        console.log("Request");
+        // while (attempt <= 5 && Object.keys(request).length === 0) {
+        // request = await amazonRequest(url);
+        //     console.log("EndRequest");
+        //     attempt++;
+        // }
         console.log("Finished");
-        res.status(200).json(request);
+        res.status(200).json(await amazonRequest(url));
     } catch (e) {
         res.status(201).json(e);
     }
