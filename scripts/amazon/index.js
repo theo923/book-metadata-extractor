fc7a09970926f2c20bfb1810f82f6866816d2fde
theo923 <theotam923@gmail.com>
2021-10-31 22:22:41 +0800

    fixes, added tutorial & timer

--- scripts/amazon/index.js
+++ scripts/amazon/index.js
@@ -70,13 +70,13 @@ export const amazonRequest = async (url) => {
         book_info["stars"] = stars;
         book_info["authors"] = authors;
         book_info["title"] = title;
-        let description;
-        let attempt = 0;
-        do {
-            description = await getDescription(url);
-            attempt++;
-        } while (description == null || !description || attempt >= 3);
-        book_info["description"] = description;
+        // let description;
+        // let attempt = 0;
+        // do {
+        //     description = await getDescription(url);
+        //     attempt++;
+        // } while (description == null || !description || attempt >= 3);
+        book_info["description"] = "";
 
         return book_info;
     } catch (e) {
